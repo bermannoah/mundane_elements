b6ac8440b7ca4c99ba9db58bbce6bb2368cb9bd2
Noah Berman <noah@wetransfer.com>
2017-06-30 14:41:44 +0200

    Adds signatures up to .bz2

--- lib/mundane_elements.ex
+++ lib/mundane_elements.ex
@@ -30,6 +30,25 @@ defmodule MundaneElements do
 
   @jxr_signature <<0x49::size(8), 0x49::size(8), 0xBC::size(8)>>
 
+  @psd_signature <<0x38::size(8), 0x42::size(8), 0x50::size(8), 0x53::size(8)>>
+
+  @epub_signature <<0x50::size(8), 0x4B::size(8), 0x3::size(8), 0x4::size(8)>> &&
+    <<0x6D::size(8), 0x69::size(8), 0x6D::size(8), 0x65::size(8), 0x74::size(8), 0x79::size(8), 0x70::size(8), 0x65::size(8), 0x61::size(8), 0x70::size(8), 0x70::size(8), 0x6C::size(8), 0x69::size(8), 0x63::size(8), 0x61::size(8), 0x74::size(8), 0x69::size(8), 0x6F::size(8), 0x6E::size(8), 0x2F::size(8), 0x65::size(8), 0x70::size(8), 0x75::size(8), 0x62::size(8), 0x2B::size(8), 0x7A::size(8), 0x69::size(8), 0x70::size(8)>>
+
+  @xpi_signature <<0x50::size(8), 0x4B::size(8), 0x3::size(8), 0x4::size(8)>> &&
+    <<0x4D::size(8), 0x45::size(8), 0x54::size(8), 0x41::size(8), 0x2D::size(8), 0x49::size(8), 0x4E::size(8), 0x46::size(8), 0x2F::size(8), 0x6D::size(8), 0x6F::size(8), 0x7A::size(8), 0x69::size(8), 0x6C::size(8), 0x6C::size(8), 0x61::size(8), 0x2E::size(8), 0x72::size(8), 0x73::size(8), 0x61::size(8)>>
+
+  @zip_signature <<0x50::size(8), 0x4B::size(8)>> && (<<0x3::size(8)>> || <<0x5::size(8)>> || <<0x7::size(8)>>) &&
+		(<<0x4::size(8)>> || <<0x6::size(8)>> || <<0x8::size(8)>>)
+
+  @tar_signature <<0x75::size(8), 0x73::size(8), 0x74::size(8), 0x61::size(8), 0x72::size(8)>>
+
+  @rar_signature <<0x52::size(8), 0x61::size(8), 0x72::size(8), 0x21::size(8), 0x1A::size(8), 0x7::size(8)>> && <<0x0::size(8)>> || <<0x1::size(8)>>
+
+  @gz_signature <<0x1F::size(8), 0x8B::size(8), 0x8::size(8)>>
+
+  @bz2_signature <<0x42::size(8), 0x5A::size(8), 0x68::size(8)>>
+
   def type(<<@png_signature, rest::binary>>), do: :png
   def type(<<@jpg_signature, rest::binary>>), do: :jpg
   def type(<<@gif_signature, rest::binary>>), do: :gif
@@ -39,6 +58,14 @@ defmodule MundaneElements do
   def type(<<@tif_signature, rest::binary>>), do: :tif
   def type(<<@bmp_signature, rest::binary>>), do: :bmp
   def type(<<@jxr_signature, rest::binary>>), do: :jxr
+  def type(<<@psd_signature, rest::binary>>), do: :psd
+  def type(<<@epub_signature, rest::binary>>), do: :epub
+  def type(<<@xpi_signature, rest::binary>>), do: :xpi
+  def type(<<@zip_signature, rest::binary>>), do: :zip
+  def type(<<@tar_signature, rest::binary>>), do: :tar
+  def type(<<@rar_signature, rest::binary>>), do: :rar
+  def type(<<@gz_signature, rest::binary>>), do: :gz
+  def type(<<@bz2_signature, rest::binary>>), do: :bz2
 
   def type(_), do: :unknown
 
--- test/mundane_elements_test.exs
+++ test/mundane_elements_test.exs
@@ -10,6 +10,14 @@ defmodule MundaneElementsTest do
   @tif_big_endian_fixture Path.join(__DIR__, "fixtures/fixture-big-endian.tif")
   @bmp_fixture Path.join(__DIR__, "fixtures/fixture.bmp")
   @jxr_fixture Path.join(__DIR__, "fixtures/fixture.jxr")
+  @psd_fixture Path.join(__DIR__, "fixtures/fixture.psd")
+  @epub_fixture Path.join(__DIR__, "fixtures/fixture.epub")
+  @xpi_fixture Path.join(__DIR__, "fixtures/fixture.xpi")
+  @zip_fixture Path.join(__DIR__, "fixtures/fixture.zip")
+  @tar_fixture Path.join(__DIR__, "fixtures/fixture.tar")
+  @rar_fixture Path.join(__DIR__, "fixtures/fixture.rar")
+  @gz_fixture Path.join(__DIR__, "fixtures/fixture.tar.gz")
+  @bz2_fixture Path.join(__DIR__, "fixtures/fixture.bz2")
 
   test ".get_file_type correctly identifies a jpg" do
     result = MundaneElements.get_file_type(@jpg_fixture)
@@ -52,5 +60,51 @@ defmodule MundaneElementsTest do
     result = MundaneElements.get_file_type(@jxr_fixture)
     assert result == :jxr
   end
+
+  test ".get_file_type correctly identifies a psd" do
+    result = MundaneElements.get_file_type(@psd_fixture)
+    assert result == :psd
+  end
+
+  @tag :skip
+  test ".get_file_type correctly identifies a epub" do
+    result = MundaneElements.get_file_type(@epub_fixture)
+    assert result == :epub
+  end
+
+  @tag :skip
+  test ".get_file_type correctly identifies an xpi" do
+    result = MundaneElements.get_file_type(@xpi_fixture)
+    assert result == :xpi
+  end
+
+  @tag :skip
+  test ".get_file_type correctly identifies a zip" do
+    result = MundaneElements.get_file_type(@zip_fixture)
+    assert result == :zip
+  end
+
+  @tag :skip
+  test ".get_file_type correctly identifies a tar" do
+    result = MundaneElements.get_file_type(@tar_fixture)
+    assert result == :tar
+  end
+
+  @tag :skip
+  test ".get_file_type correctly identifies a rar" do
+    result = MundaneElements.get_file_type(@rar_fixture)
+    assert result == :rar
+  end
+
+  test ".get_file_type correctly identifies a .tar.gz" do
+    result = MundaneElements.get_file_type(@gz_fixture)
+    assert result == :gz
+  end
+
+  test ".get_file_type correctly identifies a .bz2" do
+    result = MundaneElements.get_file_type(@bz2_fixture)
+    assert result == :bz2
+  end
+
 
 end
