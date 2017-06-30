077ce367bd38dc6d1ef9d7710afcf3bc9dc212be
Noah Berman <noah@wetransfer.com>
2017-06-30 11:57:35 +0200

    Add original fixture files and checks up to CR2
    This uses the original fixture files from the JS version of this library

--- lib/mundane_elements.ex
+++ lib/mundane_elements.ex
@@ -9,11 +9,32 @@ defmodule MundaneElements do
   end
 
 
-  @png_signature <<137::size(8), 80::size(8), 78::size(8), 71::size(8),
-  13::size(8), 10::size(8), 26::size(8), 10::size(8)>>
-  @jpg_signature <<255::size(8), 216::size(8)>>
+  @png_signature <<0x89::size(8), 0x50::size(8), 0x4E::size(8), 0x47::size(8),
+  0x0D::size(8), 0x0A::size(8), 0x1A::size(8), 0x0A::size(8)>>
+
+  @jpg_signature <<0xFF::size(8), 0xD8::size(8), 0xFF::size(8)>>
+
+  @gif_signature <<0x47::size(8), 0x49::size(8), 0x46::size(8)>>
+  # This differs from Magic_Bytes in that it checks for RIFF instead of WEBP.
+  @webp_signature <<0x52::size(8), 0x49::size(8), 0x46::size(8), 0x46::size(8)>>
+
+  @flif_signature <<0x46::size(8), 0x4C::size(8), 0x49::size(8), 0x46::size(8)>>
+
+  @cr2_signature <<0x49::size(8), 0x49::size(8), 0x2A::size(8), 0x0::size(8)>>
+  # Need to add offset to this
+  @cr2_signature_alt <<0x4D::size(8), 0x4D::size(8), 0x0::size(8), 0x2A::size(8)>>
+
+  @tif_signature <<0x49::size(8), 0x49::size(8), 0x2A::size(8), 0x0::size(8)>>
 
   def type(<<@png_signature, rest::binary>>), do: :png
   def type(<<@jpg_signature, rest::binary>>), do: :jpg
+  def type(<<@gif_signature, rest::binary>>), do: :gif
+  def type(<<@webp_signature, rest::binary>>), do: :webp
+  def type(<<@webp_signature, rest::binary>>), do: :webp
+  def type(<<@flif_signature, rest::binary>>), do: :flif
+  # Needs to be before 'tif' check
+  def type(<<@cr2_signature, rest::binary>>), do: :cr2
+  def type(<<@cr2_signature_alt, rest::binary>>), do: :cr2
+  def type(<<@tif_signature, rest::binary>>), do: :tif
   def type(_), do: :unknown
 end
--- test/mundane_elements_test.exs
+++ test/mundane_elements_test.exs
@@ -1,8 +1,12 @@
 defmodule MundaneElementsTest do
   use ExUnit.Case
 
-  @jpg_fixture Path.join(__DIR__, "fixtures/test-jpg.jpg")
-  @png_fixture Path.join(__DIR__, "fixtures/test-png.png")
+  @jpg_fixture Path.join(__DIR__, "fixtures/fixture.jpg")
+  @png_fixture Path.join(__DIR__, "fixtures/fixture.png")
+  @gif_fixture Path.join(__DIR__, "fixtures/fixture.gif")
+  @webp_fixture Path.join(__DIR__, "fixtures/fixture.webp")
+  @cr2_fixture Path.join(__DIR__, "fixtures/fixture.cr2")
+  @tif_fixture Path.join(__DIR__, "fixtures/fixture.tif")
 
   test ".get_file_type correctly identifies a jpg" do
     result = MundaneElements.get_file_type(@jpg_fixture)
@@ -13,4 +17,25 @@ defmodule MundaneElementsTest do
     result = MundaneElements.get_file_type(@png_fixture)
     assert result == :png
   end
+
+  test ".get_file_type correctly identifies a gif" do
+    result = MundaneElements.get_file_type(@gif_fixture)
+    assert result == :gif
+  end
+
+  test ".get_file_type correctly identifies a webp" do
+    result = MundaneElements.get_file_type(@webp_fixture)
+    assert result == :webp
+  end
+
+  test ".get_file_type correctly identifies a cr2" do
+    result = MundaneElements.get_file_type(@cr2_fixture)
+    assert result == :cr2
+  end
+
+  test ".get_file_type correctly identifies a tif" do
+    result = MundaneElements.get_file_type(@tif_fixture)
+    assert result == :tif
+  end
+
 end
