d00185c977fdcdddd86889fbdf2cc0c6083085cc
Noah Berman <noah@wetransfer.com>
2017-07-03 09:39:24 +0200

    Fixes some more offset issues

--- lib/mundane_elements.ex
+++ lib/mundane_elements.ex
@@ -20,13 +20,13 @@ defmodule MundaneElements do
   @flif_signature <<0x46::size(8), 0x4C::size(8), 0x49::size(8), 0x46::size(8)>>
 
   @cr2_signature <<0x49::size(8), 0x49::size(8), 0x2A::size(8), 0x0::size(8), 0x10::size(8)>> || <<0x4D::size(8), 0x4D::size(8), 0x0::size(8), 0x2A::size(8)>>
-  
+
   @cr2_signature_alt <<0x43::size(8), 0x52::size(8)>>
 
   @tif_little_endian_signature <<0x49::little-integer-size(8), 0x49::little-integer-size(8), 0x2A::little-integer-size(8), 0x00::little-integer-size(8)>>
-  
+
   @tif_big_endian_signature <<0x4D::big-integer-size(8), 0x4D::big-integer-size(8), 0x00::big-integer-size(8), 0x2A::big-integer-size(8)>>
-  
+
   @bmp_signature <<0x42::size(8), 0x4D::size(8)>>
 
   @jxr_signature <<0x49::size(8), 0x49::size(8), 0xBC::size(8)>>
@@ -40,7 +40,7 @@ defmodule MundaneElements do
   @zip_signature <<0x50::size(8), 0x4B::size(8), 0x3::size(8)>> || <<0x50::size(8), 0x4B::size(8), 0x5::size(8)>> || <<0x50::size(8), 0x4B::size(8), 0x7::size(8)>> && (<<0x4::size(8)>> || <<0x6::size(8)>> || <<0x8::size(8)>>)
 
   @tar_signature <<0x75::size(8), 0x73::size(8), 0x74::size(8), 0x61::size(8), 0x72::size(8)>>
-  
+
   @wasm_signature <<0x00::size(8), 0x61::size(8), 0x73::size(8), 0x6D::size(8)>>
 
   @rar_signature <<0x52::size(8), 0x61::size(8), 0x72::size(8), 0x21::size(8), 0x1A::size(8), 0x7::size(8), 0x0::size(8)>> || <<0x52::size(8), 0x61::size(8), 0x72::size(8), 0x21::size(8), 0x1A::size(8), 0x7::size(8), 0x1::size(8)>>
@@ -62,15 +62,15 @@ defmodule MundaneElements do
   @mp4_signature <<0x0::size(8), 0x0::size(8), 0x0::size(8), 0x18::size(8), 0x66::size(8), 0x74::size(8), 0x79::size(8), 0x70::size(8)>>
 
   @mp4_signature_1 <<0x0::size(8), 0x0::size(8), 0x0::size(8), 0x20::size(8), 0x66::size(8), 0x74::size(8), 0x79::size(8), 0x70::size(8)>>
-    
+
   @mp4_signature_2 <<0x33::size(8), 0x67::size(8), 0x70::size(8), 0x35::size(8)>>
-  
-  @mp4_signature_3 <<0x0::size(8), 0x0::size(8), 0x0::size(8), 0x1C::size(8), 0x66::size(8), 0x74::size(8), 0x79::size(8), 0x70::size(8), 0x6D::size(8), 0x70::size(8), 0x34::size(8), 0x32::size(8)>> 
-  
+
+  @mp4_signature_3 <<0x0::size(8), 0x0::size(8), 0x0::size(8), 0x1C::size(8), 0x66::size(8), 0x74::size(8), 0x79::size(8), 0x70::size(8), 0x6D::size(8), 0x70::size(8), 0x34::size(8), 0x32::size(8)>>
+
   @mp4_signature_3_with_offset <<0x6D::size(8), 0x70::size(8), 0x34::size(8), 0x31::size(8), 0x6D::size(8), 0x70::size(8), 0x34::size(8), 0x32::size(8), 0x69::size(8), 0x73::size(8), 0x6F::size(8), 0x6D::size(8)>>
-  
+
   @mp4_signature_4 <<0x0::size(8), 0x0::size(8), 0x0::size(8), 0x1C::size(8), 0x66::size(8), 0x74::size(8), 0x79::size(8), 0x70::size(8), 0x69::size(8), 0x73::size(8), 0x6F::size(8), 0x6D::size(8)>>
-  
+
   @mp4_signature_5 <<0x0::size(8), 0x0::size(8), 0x0::size(8), 0x1C::size(8), 0x66::size(8), 0x74::size(8), 0x79::size(8), 0x70::size(8), 0x6D::size(8), 0x70::size(8), 0x34::size(8), 0x32::size(8), 0x0::size(8), 0x0::size(8), 0x0::size(8), 0x0::size(8)>>
 
   @mid_signature <<0x4D::size(8), 0x54::size(8), 0x68::size(8), 0x64::size(8)>>
@@ -84,7 +84,7 @@ defmodule MundaneElements do
   @mov_signature_with_offset <<0x66::size(8), 0x72::size(8), 0x65::size(8), 0x65::size(8)>> || <<0x66::size(8), 0x74::size(8), 0x79::size(8), 0x70::size(8), 0x71::size(8), 0x74::size(8), 0x20::size(8), 0x20::size(8)>> || <<0x6D::size(8), 0x64::size(8), 0x61::size(8), 0x74::size(8)>> || <<0x77::size(8), 0x69::size(8), 0x64::size(8), 0x65::size(8)>>
 
   @avi_signature <<0x52::size(8), 0x49::size(8), 0x46::size(8), 0x46::size(8)>>
-  
+
   @avi_signature_alt <<0x41::size(8), 0x56::size(8), 0x49::size(8)>>
 
   @wmv_signature <<0x30::size(8), 0x26::size(8), 0xB2::size(8), 0x75::size(8), 0x8E::size(8), 0x66::size(8), 0xCF::size(8), 0x11::size(8), 0xA6::size(8), 0xD9::size(8)>>
@@ -98,7 +98,7 @@ defmodule MundaneElements do
   @flac_signature <<0x66::size(8), 0x4C::size(8), 0x61::size(8), 0x43::size(8)>>
 
   @wav_signature <<0x52::size(8), 0x49::size(8), 0x46::size(8), 0x46::size(8)>>
-  
+
   @wav_signature_alt <<0x57::size(8), 0x41::size(8), 0x56::size(8), 0x45::size(8)>>
 
   @amr_signature <<0x23::size(8), 0x21::size(8), 0x41::size(8), 0x4D::size(8), 0x52::size(8), 0x0A::size(8)>>
@@ -107,21 +107,21 @@ defmodule MundaneElements do
 
   @exe_signature <<0x4D::size(8), 0x5A::size(8)>>
 
-  @swf_signature <<0x43::size(8)>> || <<0x46::size(8)>>
-  
+  @swf_signature <<0x46::size(8), 0x57::size(8), 0x53::size(8)>> || <<0x43::size(8), 0x57::size(8), 0x53::size(8)>>
+
   @swf_signature_alt <<0x57::size(8), 0x53::size(8)>>
 
   @rtf_signature <<0x7B::size(8), 0x5C::size(8), 0x72::size(8), 0x74::size(8), 0x66::size(8)>>
 
-  @woff_signature <<0x77::size(8), 0x4F::size(8), 0x46::size(8), 0x46::size(8)>> 
-  
+  @woff_signature <<0x77::size(8), 0x4F::size(8), 0x46::size(8), 0x46::size(8)>>
+
   @woff_signature_alt <<0x00::size(8), 0x01::size(8), 0x00::size(8), 0x00::size(8)>> || <<0x4F::size(8), 0x54::size(8), 0x54::size(8), 0x4F::size(8)>>
 
   @woff2_signature <<0x77::size(8), 0x4F::size(8), 0x46::size(8), 0x32::size(8)>>
-  
+
   @woff2_signature_alt <<0x00::size(8), 0x01::size(8), 0x00::size(8), 0x00::size(8)>> || <<0x4F::size(8), 0x54::size(8), 0x54::size(8), 0x4F::size(8)>>
 
-  @eot_signature <<0x4c::size(8), 0x50::size(8)>> 
+  @eot_signature <<0x4c::size(8), 0x50::size(8)>>
 
   @eot_signature_alt <<0x00::size(8), 0x00::size(8), 0x01::size(8)>> || <<0x00=1::size(8), 0x00::size(8), 0x02::size(8)>> || <<0x02::size(8), 0x00::size(8), 0x02::size(8)>>
 
@@ -160,7 +160,7 @@ defmodule MundaneElements do
   @mxf_signature <<0x06::size(8), 0x0E::size(8), 0x2B::size(8), 0x34::size(8), 0x02::size(8), 0x05::size(8), 0x01::size(8), 0x01::size(8), 0x0D::size(8), 0x01::size(8), 0x02::size(8), 0x01::size(8), 0x01::size(8), 0x02::size(8)>>
 
   @mts_signature <<0x47::size(8)>>
-  
+
   @mts_signature_alt <<0x47::size(8)>> || <<0x47::size(8)>>
 
   @blend_signature <<0x42::size(8), 0x4C::size(8), 0x45::size(8), 0x4E::size(8), 0x44::size(8), 0x45::size(8), 0x52::size(8)>>
@@ -205,21 +205,18 @@ defmodule MundaneElements do
   def type(<<@webm_signature, rest::binary>>), do: :webm
   def type(<<@mov_signature, rest::binary>>), do: :mov
   def type(<<_, _, _, _, @mov_signature_with_offset, rest::binary>>), do: :mov
-  # The AVI check below is missing the first part of the signature and may not be safe to use.
-  def type(<<_, _, _, _, _, _, _, _, @avi_signature_alt, rest::binary>>), do: :avi 
+  def type(<<@avi_signature, _, _, _, _, @avi_signature_alt, rest::binary>>), do: :avi
   def type(<<@wmv_signature, rest::binary>>), do: :wmv
   def type(<<@mp3_signature, rest::binary>>), do: :mp3
   def type(<<_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, @opus_signature, rest::binary>>), do: :opus
   def type(<<@ogg_signature, rest::binary>>), do: :ogg
   def type(<<@flac_signature, rest::binary>>), do: :flac
-  # The WAV check below is missing the first part of the signature and may not be safe to use.
-  def type(<<_, _, _, _, _, _, _, _, @wav_signature_alt, rest::binary>>), do: :wav
+  def type(<<@wav_signature, _, _, _, _, @wav_signature_alt, rest::binary>>), do: :wav
   def type(<<@amr_signature, rest::binary>>), do: :amr
   def type(<<@pdf_signature, rest::binary>>), do: :pdf
   def type(<<@exe_signature, rest::binary>>), do: :exe
-  def type(<<@crx_signature, rest::binary>>), do: :crx 
-  # The SWF check below is missing the first part of the signature and may not be safe to use.
-  def type(<<_, @swf_signature_alt, rest::binary>>), do: :swf
+  def type(<<@crx_signature, rest::binary>>), do: :crx
+  def type(<<@swf_signature, rest::binary>>), do: :swf
   def type(<<@rtf_signature, rest::binary>>), do: :rtf
   def type(<<@wasm_signature, rest::binary>>), do: :wasm
   def type(<<@woff_signature, @woff_signature_alt, rest::binary>>), do: :woff
